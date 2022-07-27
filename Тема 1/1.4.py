d1c0ac8a49a56c593bb408253efa4f3c400d0a52
SetAblaze <88194169+SetAblaze@users.noreply.github.com>
2022-07-27 21:06:45 +0300

    Задание 1

---
+++ Тема 1/1.4.py
@@ -0,0 +1,13 @@
+a = input('Введите значение ')
+b = input('Введите значение ')
+c = input('Введите значение ')
+
+buff = a
+
+print (f'{a} {b} {c}')
+
+a = b
+b = c
+c = buff
+
+print (f'{a} {b} {c}')
