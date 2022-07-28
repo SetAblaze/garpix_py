fafeef57e89f979b55cd372079f7f9c88471343f
SetAblaze <88194169+SetAblaze@users.noreply.github.com>
2022-07-28 15:03:56 +0300

    Задание 3 упрощено

--- Тема 3/3.6.py
+++ Тема 3/3.6.py
@@ -1,10 +1,8 @@
 reps_num = int(input('Введите количество повторений '))
 fib_num = 1
 prev_num = 0
-buff = 0
+
 
 for i in range(reps_num):
-    buff = fib_num
-    fib_num += prev_num
-    prev_num = buff
-    print('$ ' * (fib_num))
+    fib_num, prev_num = fib_num + prev_num, fib_num
+    print('$ ' * fib_num)
