f8e98cc2ed3034315a7d02b1dea0709e41f466e0
SetAblaze <88194169+SetAblaze@users.noreply.github.com>
2022-08-07 11:58:53 +0300

    Задание 4 исправление

--- Тема 4/4.4.py
+++ Тема 4/4.4.py
@@ -1,9 +1,6 @@
 my_list = [1, 2, 1, 5, 3, 1, 4, 3, 5, 5]
 print(my_list)
 
-for i in my_list:
-    if my_list.count(i) > 1:
-        for j in range(my_list.count(i) - 1):
-            my_list.remove(i)
+my_list = list(set(my_list))
 
 print(my_list)
