4c064973606efa262de5bf94725cf525bc9fbd34
SetAblaze <88194169+SetAblaze@users.noreply.github.com>
2022-08-07 14:18:26 +0300

    Задание 4 исправлено

--- Тема 4/4.5.py
+++ Тема 4/4.5.py
@@ -1,16 +1,11 @@
 lst1 = ['Санкт-Петербург', 'Москва', 'Казань', ]
 lst2 = ['Воронеж', 'Санкт-Петербург', 'Иваново', 'Жуковский']
-my_list1 = []
-my_list2 = []
 
-for i in lst1:
-    for j in lst2:
-        if i == j:
-            my_list1.append(i)
-        if lst2.count(i) == 0 and my_list2.count(i) < 1:
-            my_list2.append(i)
-        elif lst1.count(j) == 0 and my_list2.count(j) < 1:
-            my_list2.append(j)
+my_list1 = set(lst1)
+my_list1.intersection_update(lst1, lst2)
+my_list2 = set(lst2)
+my_list2.update(lst1)
+my_list2.difference_update(my_list1)
 
 print(my_list1)
 print(my_list2)
