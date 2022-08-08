522d49bd9616ac50f2d6d95301d5bf1629436961
SetAblaze <88194169+SetAblaze@users.noreply.github.com>
2022-08-08 21:23:54 +0300

    Задание 9

--- Тема 9/9.1.py
+++ Тема 9/9.1.py
@@ -1,7 +1,7 @@
 import requests
 
 def get_req(generic_url):
-    response = f'По запросу {generic_url} GET вывело следующий код: {requests.get(generic_url)}'
+    response = f'По запросу {generic_url} GET вывело следующий код:  {requests.get(generic_url)}'
     return response
 
 print(get_req('https://www.google.ru/'))
