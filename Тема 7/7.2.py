92014d1cc479fb33a868272a59f34e2d220a9738
SetAblaze <88194169+SetAblaze@users.noreply.github.com>
2022-08-09 22:58:05 +0300

    Задание 7 исправлено

--- Тема 7/7.2.py
+++ Тема 7/7.2.py
@@ -1,7 +1,7 @@
 def read_last(lines, file):
     with open(file, encoding='utf=8', mode='r') as f:
-        output_file = f.readlines()
-        for i in range(lines):
-            print(output_file[len(output_file) - 1 - i])
+        last_line = f.readlines()
+        for i in range(-1, -lines - 1, -1):
+            print(last_line[i])
 
 read_last(4, r'./test_data/data')
