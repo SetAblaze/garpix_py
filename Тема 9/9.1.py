fa728d2b866b4f11a4601777e05fb120f8a6b17e
SetAblaze <88194169+SetAblaze@users.noreply.github.com>
2022-08-09 22:40:33 +0300

    Задание 9 исправлено

--- Тема 9/9.1.py
+++ Тема 9/9.1.py
@@ -2,6 +2,6 @@ import requests
 
 def get_req(generic_url):
         response = requests.get(generic_url)
-        print(f'По запросу {generic_url} GET вывело следующий код: {response.status_code}')
+        print(f'По запросу {generic_url} GET вывело следующий код:  {response.status_code}')
 
 get_req('https://www.google.ru/')
