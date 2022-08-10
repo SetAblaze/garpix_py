bd381dc63ec94bb38aba94798259f6ce24478717
SetAblaze <88194169+SetAblaze@users.noreply.github.com>
2022-08-10 22:33:33 +0300

    Задание 9

--- Тема 9/9.2.py
+++ Тема 9/9.2.py
@@ -1,13 +1,21 @@
 import requests
 
 def req_func(generic_url, args_arr):
-    response = ''
     if(generic_url[len(generic_url) - 1] == '?'):
         response = requests.get(generic_url, params=args_arr)
         return response.url
     else:
-        for
+        output_url = ''
+        buff_url = generic_url
+        for i in args_arr:
+            index = buff_url.find('=') + 1
+            output_url += buff_url[:index] + args_arr[i]
+            buff_url = buff_url[index+1:]
+        response = requests.get(output_url)
+        return output_url, response.status_code
 
 
-payload = {'key1': 'value1', 'key2': 'value2'}
+
+
+payload = {'key1': 'value1', 'key2': 'value2', 'key3': 'value3'}
 print(req_func('https://www.ozon.ru/search/?from_global=@&sorting=@&text=@', payload))
