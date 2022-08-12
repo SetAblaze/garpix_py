9d910c9978bc1cb581eb26976e5d361f131844f7
SetAblaze <88194169+SetAblaze@users.noreply.github.com>
2022-08-12 15:15:47 +0300

    Задание 9

--- Тема 9/9.3.py
+++ Тема 9/9.3.py
@@ -1,7 +1,14 @@
+import requests
+
 def some_func(language_list):
     output_arr = []
-
-
+    for i in language_list:
+        response = requests.get(
+            'https://api.github.com/search/repositories',
+            params={'q': f'requests+language:{i}'},
+        )
+        jsn = response.json()
+        output_arr.append([i,jsn['total_count']])
     return output_arr
 
 print(some_func(['python', 'c++', 'java', 'javascript', 'ruby', 'c#']))
