8537bd28ae6931eba18550fa33dc7ccae1bb51c9
SetAblaze <88194169+SetAblaze@users.noreply.github.com>
2022-08-13 12:33:47 +0300

    Задание 9

---
+++ Тема 10/10.1.py
@@ -0,0 +1,12 @@
+class MyException(Exception):
+    def __init__(self, message):
+        self.txt = message
+
+def check_name(name):
+    for i in name:
+        if i.isdigit():
+            raise MyException('Имя не может содержать в себе цифры')
+    print(name)
+
+check_name('Vova')
+check_name('Vova2123')
